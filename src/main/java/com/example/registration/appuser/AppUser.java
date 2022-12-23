bfac2a49c2f5a8364dfaacb494e6075d36c76b6a
Oyewale Abdulsobur <66224527+smilingprogrammer@users.noreply.github.com>
2022-12-23 16:16:14 +0100

    Setting app user

--- src/main/java/com/example/registration/appuser/AppUser.java
+++ src/main/java/com/example/registration/appuser/AppUser.java
@@ -31,11 +31,26 @@ public class AppUser implements UserDetails {
     private String username;
     private String email;
     private String password;
-
     @Enumerated(EnumType.STRING)
     private AppUserRole appUserRole;
     private Boolean locked;
     private Boolean enabled;
+
+    public AppUser(String name,
+                   String username,
+                   String email,
+                   String password,
+                   AppUserRole appUserRole,
+                   Boolean locked,
+                   Boolean enabled) {
+        this.name = name;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
--- src/main/java/com/example/registration/appuser/AppUserService.java
+++ src/main/java/com/example/registration/appuser/AppUserService.java
@@ -19,6 +19,7 @@ public class AppUserService implements UserDetailsService {
             throws UsernameNotFoundException {
         return appUserRepository.findByEmail(email)
                 .orElseThrow(() ->
-                        new UsernameNotFoundException(""));
+                        new UsernameNotFoundException(
+                                String.format(USER_NOT_FOUND_MSG, email)));
     }
 }
